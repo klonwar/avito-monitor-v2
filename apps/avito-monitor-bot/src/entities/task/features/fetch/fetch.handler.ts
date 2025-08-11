1d8f94abd84d87c7cecfc7dfb39a482347c8ada2
Dmitry Arapov <dima.arapov@gmail.com>
2025-08-11 14:32:27 +0300

    fix: enable local priority for cars

--- apps/avito-monitor-bot/src/entities/task/features/fetch/fetch.handler.ts
+++ apps/avito-monitor-bot/src/entities/task/features/fetch/fetch.handler.ts
@@ -1,3 +1,5 @@
+import { Page } from 'puppeteer'
+
 import { Logger } from '@nestjs/common'
 import type { ICommandHandler } from '@nestjs/cqrs'
 import { CommandHandler } from '@nestjs/cqrs'
@@ -21,6 +23,48 @@ export class FetchHandler implements ICommandHandler<FetchCommand> {
 
     const response = await page.goto(link.url)
 
-    return !!response && response.ok()
+    if (!response || !response.ok()) return false
+
+    const needToEnableLocalPriority = !!(await page.$(
+      `[data-marker="filters/localPriority/localPriority"][aria-checked="false"]`,
+    ))
+
+    if (needToEnableLocalPriority) {
+      const localPriorityEnabled = await this.enableLocalPriority(page)
+
+      if (!localPriorityEnabled) {
+        this.logger.warn(`Failed to enable local priority for link ${link.id}`)
+        return false
+      }
+    }
+
+    return true
+  }
+
+  private async enableLocalPriority(page: Page): Promise<boolean> {
+    const location = await page.$(`[data-marker="search-form/change-location"]`)
+
+    if (!location) return false
+
+    await location.click()
+
+    await page.waitForSelector('[class*="popup-localPriority"]', { timeout: 5000 })
+    const checkbox = await page.$('[class*="popup-localPriority"]')
+
+    await checkbox!.click()
+
+    const button = await page.$(`[data-marker="popup-location/save-button"]`)
+
+    if (!button) return false
+
+    await button.click()
+
+    await page.waitForNavigation()
+
+    const localPriorityEnabled = await page.$(
+      `[data-marker="filters/localPriority/localPriority"][aria-checked="true"]`,
+    )
+
+    return !!localPriorityEnabled
   }
 }
